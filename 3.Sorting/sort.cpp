2169e3bb77d6152f53133bf61e6d9d2bea908319
Vasudevshetty <vasudeepu2815@gmail.com>
2024-05-23 11:18:51 +0530

    Added the anayaliss part should compelte all the problems asap.

--- 3.Sorting/sort.cpp
+++ 3.Sorting/sort.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <random>
 using namespace std;
 
-pair<vector<int>, int> bubbleSort(vector<int> &array)
+#define MIN 10
+#define MAX 1000
+
+vector<int> generateRandomArray(int size, int minVal, int maxVal, bool unique = false)
+{
+    vector<int> array;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(minVal, maxVal);
+
+    if (unique)
+    {
+        if (maxVal - minVal + 1 < size)
+        {
+            cerr << "Range too small for the size of the array with unique elements";
+            return array;
+        }
+
+        vector<int> pool;
+        for (int i = minVal; i <= maxVal; i++)
+            pool.push_back(i);
+
+        shuffle(pool.begin(), pool.end(), gen);
+        array.assign(pool.begin(), pool.begin() + size);
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+            array.push_back(distr(gen));
+    }
+    return array;
+}
+
+pair<vector<int>, int> bubbleSort(vector<int> array)
 {
     int count = 0;
     int n = array.size();
@@ -19,7 +55,7 @@ pair<vector<int>, int> bubbleSort(vector<int> &array)
     return {array, count};
 }
 
-pair<vector<int>, int> insertionSort(vector<int> &array)
+pair<vector<int>, int> insertionSort(vector<int> array)
 {
     int count = 0;
     int n = array.size();
@@ -38,7 +74,7 @@ pair<vector<int>, int> insertionSort(vector<int> &array)
     return {array, count};
 }
 
-pair<vector<int>, int> selectionSort(vector<int> &array)
+pair<vector<int>, int> selectionSort(vector<int> array)
 {
     int count = 0;
     int n = array.size();
@@ -58,9 +94,23 @@ pair<vector<int>, int> selectionSort(vector<int> &array)
 
 int main()
 {
-    vector<int> array({2, 8, 1, 3, 4, 9, 7, 6, 10, 5});
-    insertionSort(array);
-    for (auto ele : array)
-        cout << ele << " ";
-    cout << endl;
+    ofstream dataFile("../data/sort_data.dat");
+    if (!dataFile.is_open())
+    {
+        cerr << "Unable to open the file to write";
+        return -1;
+    }
+
+    dataFile << "# input size | bubble sort | insertion sort | selection sort" << endl;
+
+    for (int size = 100; size <= 1000; size += 100)
+    {
+        int bubble_count = bubbleSort(generateRandomArray(size, MIN, MAX)).second;
+        int insertion_count = insertionSort(generateRandomArray(size, MIN, MAX)).second;
+        int selection_count = bubbleSort(generateRandomArray(size, MIN, MAX)).second;
+
+        dataFile << size << " " << bubble_count << " " << insertion_count << " " << selection_count << " " << endl;
+    }
+    dataFile.close();
+    return 0;
 }
