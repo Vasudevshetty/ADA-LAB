92b5e9e536d77edb62273230e1c27037590a6693
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-19 06:51:38 +0530

    Done witht he merge and quick sort too, anaylsed well, should cofirm the grpahs generated. The program file structure is such that any sorting technique can be added asap with just one inclusion.

--- 3.Sorting/Sorting.java
+++ 3.Sorting/Sorting.java
@@ -55,6 +55,85 @@ public class Sorting {
         return count;
     }
 
+    private static int quickSort(int[] array) {
+        return quickSort(array, 0, array.length - 1);
+    }
+
+    private static int quickSort(int[] array, int low, int high) {
+        int count = 0;
+        if (low < high) {
+            int[] partitionResult = partition(array, low, high);
+            int pi = partitionResult[0];
+            count += partitionResult[1];
+            count += quickSort(array, low, pi - 1);
+            count += quickSort(array, pi + 1, high);
+        }
+        return count;
+    }
+
+    private static int[] partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        int count = 0;
+        for (int j = low; j < high; j++) {
+            count++;
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return new int[] { i + 1, count };
+    }
+
+    private static int mergeSort(int[] array) {
+        return mergeSort(array, 0, array.length - 1);
+    }
+
+    private static int mergeSort(int[] array, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count += mergeSort(array, left, mid);
+            count += mergeSort(array, mid + 1, right);
+            count += merge(array, left, mid, right);
+        }
+        return count;
+    }
+
+    private static int merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(array, left, L, 0, n1);
+        System.arraycopy(array, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        int count = 0;
+
+        while (i < n1 && j < n2) {
+            count++;
+            if (L[i] <= R[j]) {
+                array[k++] = L[i++];
+            } else {
+                array[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) {
+            array[k++] = L[i++];
+        }
+
+        while (j < n2) {
+            array[k++] = R[j++];
+        }
+
+        return count;
+    }
+
     public static int sort(int[] array, String sortType) {
         if (sortType.equals("bubble"))
             return bubbleSort(array);
@@ -62,7 +141,10 @@ public class Sorting {
             return selectionSort(array);
         if (sortType.equals("insertion"))
             return insertionSort(array);
-
+        if (sortType.equals("merge"))
+            return mergeSort(array);
+        if (sortType.equals("quick"))
+            return quickSort(array);
         return -1;
     }
 }
